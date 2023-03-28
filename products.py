0e10ea605762ceea3b2537d773341ae724361f2e
Chris Kuo <chris1680888@gmail.com>
2023-03-28 21:44:30 +0800

    Track Expenses

--- products.py
+++ products.py
@@ -6,7 +6,12 @@ while True:
     if item == 'q':
         break
     price = input('請輸入花費金額: ')
+    price = int(price)
     products.append([item, price])
 
 for p in products:
     print('商品', p[0], '的花費是', p[1], '元')
+
+with open('products.csv', 'w') as f:        #打開檔案
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')   #寫入檔案
