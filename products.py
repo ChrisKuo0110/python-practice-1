e40cc46909aacf3863bfeb066e16dd4c30d44032
Chris Kuo <chris1680888@gmail.com>
2023-03-28 22:24:10 +0800

    Track Expenses

--- products.py
+++ products.py
@@ -12,6 +12,9 @@ while True:
 for p in products:
     print('商品', p[0], '的花費是', p[1], '元')
 
-with open('products.csv', 'w') as f:        #打開檔案
+with open('products.csv', 'w') as f:                     #打開檔案
+#如果中文欄位名讀取有問題可以額外加, encoding = 'utf-8'
+#變成with open('products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,價格\n')                                #欄位名稱
     for p in products:
-        f.write(p[0] + ',' + str(p[1]) + '\n')   #寫入檔案
+        f.write(p[0] + ',' + str(p[1]) + '\n')           #寫入檔案
